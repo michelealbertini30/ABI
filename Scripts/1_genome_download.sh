d06ac3763ed10aa5f10a990feaf85824360e58f3
michelealbertini30 <michelealbertini30@gmail.com>
2026-04-27 14:48:15 +0200

    deleted

--- Scripts/1_genome_download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ref=$(awk '{print $1}' $1 | grep -v "#" | tr "\n" " ")
-ref=${ref%?}
-
-for rf in $ref
-	do
-		datasets download genome accession $rf --include genome --dehydrated --filename $rf.zip
-	done
